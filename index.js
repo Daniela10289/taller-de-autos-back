90ed79d3024668959cf33f1e10a905fd2c229850
daniela <dpalacio2200@gmail.com>
2022-04-21 18:41:04 -0500

    Configuraciones generales en el  index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hola mi server en express');
+    res.send('Server en express');
   })
 
 app.listen(port, () => {
